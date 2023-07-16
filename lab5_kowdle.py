52f41024fd988e678003ad5b8e580692d8dd3ade
StormWolf49 <kowdlen@gmail.com>
2023-07-16 16:19:23 -0700

    Commented Lab 5

--- lab5_kowdle.py
+++ lab5_kowdle.py
@@ -12,6 +12,7 @@ t = turtle.Turtle()
 t.speed(0)
 colors = ['turquoise', 'tomato', 'lime green', 'dark orange', 'cornflower blue', 'fuchsia']
 
+#Sets the background of the drawing in a way that will be saved in the postscript.
 def draw_background(a_turtle):
     """ Draw a background rectangle. """
     ts = a_turtle.getscreen()
@@ -45,25 +46,25 @@ def draw_background(a_turtle):
     a_turtle.setheading(turtleheading)
     a_turtle.speed(turtlespeed)
 
+#Creates a hexagon with a gradient from the white center to the black border, simulating a light diffusing down a hexagonal hallway.
 def light():
-    color = 0
-
     for i in range(0, 255, 3):
-        nc = color + i
         t.penup()
         t.home()
-        t.forward(250-(2.6*(i/3)))
+        
+        t.forward(285-(3*(i/3)))
         t.left(90)
-        t.pencolor((nc, nc, nc))
-        t.fillcolor((nc, nc, nc))
+        t.pencolor((i, i, i))
+        t.fillcolor((i, i, i))
         t.begin_fill()
         t.pendown()
-        t.circle(250-(2.6*(i/3)), steps=6)
+        t.circle(285-(3*(i/3)), steps=6)
         t.end_fill()
 
     t.penup()
     t.home()
 
+#Creates a hexgaonal hallway by creating multiple conentric hexagons each slightly larger than the last, with each side being a different color.
 def tunnel():
     for i in range(7):
         t.pencolor(colors[i%6])
@@ -73,10 +74,9 @@ def tunnel():
         t.pendown()
         t.left(60)
     
-    
     t.penup()
     t.home()
-    for i in range(350):
+    for i in range(400):
         if i%6 == 0:
             t.penup()
             t.home()
@@ -90,20 +90,44 @@ def tunnel():
 
     t.penup()
     t.home()
-    t.forward(10)
+    t.forward(15)
     t.left(90)
     t.pencolor('white')
     t.fillcolor('white')
     t.begin_fill()
     t.pendown()
-    t.circle(10, steps=6)
+    t.circle(15, steps=6)
     t.end_fill()
 
+#Creates a glowing golden orb at the end of the hallway, by creating a gradient golden circle with a white circle at the center of the gradient.
+def orb():
+    t.penup()
+    t.home()
+
+    for i in range(5):
+        g = 215 + (i*8)
+        b = i*51
+        t.penup()
+        t.home()
+        t.forward(10-i)
+        t.left(90)
+        t.pencolor((255, g, b))
+        t.fillcolor((255, g, b))
+        t.begin_fill()
+        t.pendown()
+        t.circle(10-i)
+        t.end_fill()
+
+    t.penup()
+    t.home()
+
+#Executes the various draing functions and then saves the image as postscript, and then converts it into a jpg.
 def main():
     turtle.bgcolor('black')
     draw_background(t)
     light()
     tunnel()
+    orb()
     t.home()
     t.hideturtle()
 
