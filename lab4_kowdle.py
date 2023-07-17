154aee47912cd7db6326a36f5c1d2b38b552eeba
Nikhil Kowdle <72424123+StormWolf49@users.noreply.github.com>
2023-07-17 12:06:34 -0700

    Lab 4 Test

--- lab4_kowdle.py
+++ lab4_kowdle.py
@@ -13,11 +13,47 @@ def count_letters (sentence) :
             lower_case += 1
     return upper_case, lower_case
 
+'''
+Test 1
+The quick Brow Fox
+No. of Upper case characters: 3 
+No. of Lower case character: 12
+
+Test 2
+
+No. of Upper case characters: 0 
+No. of Lower case character: 0
+
+Test 3
+why are we whispering
+No. of Upper case characters: 0 
+No. of Lower case character: 18
+
+Test 4
+WHY ARE WE SCREAMING
+No. of Upper case characters: 17 
+No. of Lower case character: 0
+'''
+
 #Task 2
 #Caluclates the final price of an item when given the price, any discount, and tax rate
 def price_calculator (original_price, discount = 0, tax_rate = 0.08) :
     price = round((original_price * (1 - discount)) * (1 + tax_rate), 2)
     return price
+
+'''
+Test 1
+Original Price: 100, discount 20%, tax rate 8%, final price 86.4
+
+Test 2
+Original Price: 523, discount 0%, tax rate 9.5%, final price 572.68
+
+Test 3
+Original Price: 250, discount 35%, tax rate 0%, final price 162.5
+
+Test 4
+Original Price: 420, discount 69%, tax rate 7%, final price 139.31
+'''
 
 #Task 3
 #Formats any given string arguments into a single formatted list, with each argument on its own line when printed.
@@ -27,6 +63,20 @@ def address_book (*args) :
         combined_addresses += arg + '\n'
     return combined_addresses
 
+'''
+Test 1
+Lan Cal Poly Pomona
+Kay CSUFullerton
+Jane Microsoft
+
+Test 2
+John Doe
+Jane Doe
+The Batman
+The Guy in the Broken yet suprisngly Ergonomic Chair
+Fred
+'''
+
 def main() :
     #Task 1 Main
     upper, lower = count_letters('The quick Brow Fox')
@@ -35,11 +85,11 @@ def main() :
     upper, lower = count_letters('')
     print('')
     print(f"No. of Upper case characters: {upper} \nNo. of Lower case character: {lower}")
-    upper, lower = count_letters('test')
-    print('test')
+    upper, lower = count_letters('why are we whispering')
+    print('why are we whispering')
     print(f"No. of Upper case characters: {upper} \nNo. of Lower case character: {lower}")
-    upper, lower = count_letters('TEST')
-    print('TEST')
+    upper, lower = count_letters('WHY ARE WE SCREAMING')
+    print('WHY ARE WE SCREAMING')
     print(f"No. of Upper case characters: {upper} \nNo. of Lower case character: {lower}")
 
     print()
@@ -55,6 +105,6 @@ def main() :
     #Task 3 Main
     print(address_book(''))
     print(address_book ("Lan Cal Poly Pomona", "Kay CSUFullerton", "Jane Microsoft"))
-    print(address_book("Five", "Quatre", "Tres", "II", "1"))
+    print(address_book("John Doe", "Jane Doe", "The Batman", "The Guy in the Broken yet suprisngly Ergonomic Chair", "Fred "))
 
 main()
