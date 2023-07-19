87d83dd0abc7b0be429490b952754ed681fce12d
Nikhil Kowdle <72424123+StormWolf49@users.noreply.github.com>
2023-07-18 17:44:02 -0700

    Lab 6 Task 1

--- lab6_kowdle.py
+++ lab6_kowdle.py
@@ -3,7 +3,19 @@
 #This program contains the task required for this assignment.
 
 #Task 1
+def L1():
+    list = []
+    n = float(input("Please enter a positive number: "))
+    while n >= 0:        
+        list.append(n)
+        n = float(input("Please enter a positive number: "))
+    print(list)
 
+import random
+def L2():
+    n = int(input("Please enter a positive integer: "))
+    list = [round(random.uniform(1, 10), 2) for _ in range(n)]
+    print(list)
 
 '''
 
@@ -21,3 +33,9 @@
 '''
 
 '''
+
+def main():
+    L1()
+    L2()
+
+main()
